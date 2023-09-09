8cb72954dcfed430e387413173e61c5dfc6a4686
Evgenii Eskov <e.s.eskov@gmail.com>
2023-09-09 15:10:52 +0200

    add prettier package

--- src/services/codeInterpreter.ts
+++ src/services/codeInterpreter.ts
@@ -5,16 +5,20 @@ export class InputError extends Error {}
 export class CodeError extends Error {}
 
 export function runCode(code: string, input: string): unknown {
+  const parseInput = (s: string) => JSON.parse(s);
+
   try {
-    const parsedInput = input.split('\n').map((s) => JSON.parse(s));
-    try {
-      const wrapper = new Function('v', `return ${code}`);
-      const func = wrapper(v);
-      return func.apply(func, parsedInput);
-    } catch (e: unknown) {
-      if (isErrorLike(e)) throw new CodeError(e.message);
+    const parsedInput = input.split('\n').map(parseInput);
+    const wrapper = new Function('v', `return ${code}`);
+    const func = wrapper(v);
+    return func.apply(func, parsedInput);
+  } catch (error) {
+    if (isErrorLike(error)) {
+      if (error instanceof CodeError) {
+        throw error;
+      } else {
+        throw new InputError(error.message);
+      }
     }
-  } catch (e: unknown) {
-    if (isErrorLike(e)) throw new InputError(e.message);
   }
 }
